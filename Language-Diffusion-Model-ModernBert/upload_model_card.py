9d6bff4766b6f2ade37ca1e6d64b4079075609ef
Jaydeep <j.raijada25@gmail.com>
2026-05-01 15:57:15 +0530

    Upload both GIFs and model card to HF

--- Language-Diffusion-Model-ModernBert/upload_model_card.py
+++ Language-Diffusion-Model-ModernBert/upload_model_card.py
@@ -2,6 +2,7 @@ from huggingface_hub import HfApi
 import os
 
 api = HfApi(token=os.environ["HF_TOKEN"])
-api.upload_file(path_or_fileobj="inference.gif", path_in_repo="inference.gif", repo_id="JaydeepR/ldm-modernbert-base-sft")
+api.upload_file(path_or_fileobj="inference_pretrain.gif", path_in_repo="inference.gif", repo_id="JaydeepR/ldm-modernbert-base-sft")
+api.upload_file(path_or_fileobj="inference_sft.gif", path_in_repo="inference_sft.gif", repo_id="JaydeepR/ldm-modernbert-base-sft")
 api.upload_file(path_or_fileobj="MODEL_CARD.md", path_in_repo="README.md", repo_id="JaydeepR/ldm-modernbert-base-sft")
 print("Done!")
